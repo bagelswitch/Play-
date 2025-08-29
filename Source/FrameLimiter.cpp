8bce69abc9ee0ea23392a3c24ebe912d0b256be3
bagelswitch <5595627+bagelswitch@users.noreply.github.com>
2025-08-29 07:20:30 -0700

    Fully revert all frame limiter changes. The old ways are better.

--- Source/FrameLimiter.cpp
+++ Source/FrameLimiter.cpp
@@ -2,63 +2,87 @@
 #include <cassert>
 #include <thread>
 
-LARGE_INTEGER ft = {};
-
 CFrameLimiter::CFrameLimiter()
 {
+#ifdef _WIN32
 	timeBeginPeriod(1);
 	m_timer = CreateWaitableTimer(NULL, TRUE, NULL);
+#endif
+	for(uint32 i = 0; i < MAX_FRAMETIMES; i++)
+	{
+		m_frameTimes[i] = std::chrono::microseconds(0);
+	}
 }
 
 CFrameLimiter::~CFrameLimiter()
 {
+#ifdef _WIN32
 	CloseHandle(m_timer);
 	timeEndPeriod(1);
+#endif
 }
 
 void CFrameLimiter::BeginFrame()
 {
 	assert(!m_frameStarted);
 	m_lastFrameTime = std::chrono::high_resolution_clock::now();
-	if(m_delay && m_enabled)
-	{
-		SetWaitableTimer(m_timer, &ft, 0, NULL, NULL, 0);
-		m_active = true;
-	}
-	else {
-		m_active = false;
-	}
 	m_frameStarted = true;
 }
 
 void CFrameLimiter::EndFrame()
 {
 	assert(m_frameStarted);
-	m_lastFrameDuration = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - m_lastFrameTime);
-	m_delay = (m_minFrameDuration - m_lastFrameDuration) > m_graceUsec;
-	if(m_active && m_delay && m_enabled)
+
+	//Add current frame time to array
 	{
-		WaitForSingleObject(m_timer, INFINITE);
+		auto currentFrameTime = std::chrono::high_resolution_clock::now();
+		auto frameDuration = std::chrono::duration_cast<std::chrono::microseconds>(currentFrameTime - m_lastFrameTime);
+		m_frameTimes[m_frameTimeIndex++] = frameDuration;
+		m_frameTimeIndex %= MAX_FRAMETIMES;
+	}
+
+	//Compute average frame time
+	std::chrono::microseconds averageFrameTime = std::chrono::microseconds(0);
+	for(uint32 i = 0; i < MAX_FRAMETIMES; i++)
+	{
+		averageFrameTime += m_frameTimes[i];
+	}
+	averageFrameTime /= MAX_FRAMETIMES;
+
+	if(averageFrameTime < m_minFrameDuration)
+	{
+		auto delay = m_minFrameDuration - averageFrameTime;
+#ifdef _WIN32
+		{
+			LARGE_INTEGER ft = {};
+			ft.QuadPart = -static_cast<int64>(delay.count() * 10);
+			SetWaitableTimer(m_timer, &ft, 0, NULL, NULL, 0);
+			WaitForSingleObject(m_timer, INFINITE);
+		}
+#elif defined(__APPLE__) || defined(__EMSCRIPTEN__)
+		//Sleeping for the whole delay on some platforms doesn't provide a good enough resolution
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		auto targetTime = currentTime + delay;
+		while(currentTime < targetTime)
+		{
+			std::this_thread::sleep_for(std::chrono::microseconds(250));
+			currentTime = std::chrono::high_resolution_clock::now();
+		}
+#else
+		std::this_thread::sleep_for(delay);
+#endif
 	}
 	m_frameStarted = false;
 }
 
 void CFrameLimiter::SetFrameRate(uint32 fps)
 {
-	if (fps == 60) {
-		fps = 62;
-	}
-
 	if(fps == 0)
 	{
 		m_minFrameDuration = std::chrono::microseconds(0);
-		m_enabled = false;
 	}
 	else
 	{
 		m_minFrameDuration = std::chrono::microseconds(1000000 / fps);
-		m_enabled = true;
 	}
-
-	ft.QuadPart = -(m_minFrameDuration.count() * 10);
 }
