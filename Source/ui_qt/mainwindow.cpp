9ff86dd349d2dba83bb0845a5a6f4ad3b48c3bfa
bagelswitch <5595627+bagelswitch@users.noreply.github.com>
2025-09-01 07:33:53 -0700

    update title-specific OGL scaling factors

--- Source/ui_qt/mainwindow.cpp
+++ Source/ui_qt/mainwindow.cpp
@@ -458,9 +458,10 @@ void MainWindow::BootArcadeMachine(fs::path arcadeDefPath)
 		ogl_scale_factors["superdbz"] = 4; // works at all scaling factors
 		ogl_scale_factors["fateulc"] = 4;  // works at all scaling factors
 		ogl_scale_factors["acedriv3"] = 4; // works at all scaling factors
+		ogl_scale_factors["soulclb3"] = 4; // works at all scaling factors
 		ogl_scale_factors["fghtjam"] = 2; // scale factor 2 fixes mis-aligned character sprite segments
 		ogl_scale_factors["sbxc"] = 1; // scale factor 1 fixes mis-aligned first column in opening video
-		ogl_scale_factors["tekken51"] = 1; // scale factor 1 fixes vertical black lines
+		ogl_scale_factors["tekken51"] = 1; // scale factor 1 fixes mis-colored textures
 		ogl_scale_factors["motogp"] = 1;   // scale factor 1 fixes dashed horizontal black lines in background tiles
 		ogl_scale_factors["default"] = 1;
 
